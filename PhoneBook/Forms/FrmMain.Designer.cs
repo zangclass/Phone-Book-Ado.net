66dc0ed065ffd91123aedc0c4c0406c7303a094b
Ebrahim <Ebrahim@DESKTOP-5ND39I7>
2021-03-17 15:35:08 +0330

    طراحی گرافیکی فرم اصلی

---
+++ PhoneBook/Forms/FrmMain.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace PhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnShahr = new System.Windows.Forms.Button();
+            this.btnOstan = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDarbareyMa = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(838, 306);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Teal;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(777, 75);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(112, 37);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "جستجو";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // btnShahr
+            // 
+            this.btnShahr.BackColor = System.Drawing.Color.Teal;
+            this.btnShahr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShahr.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnShahr.ForeColor = System.Drawing.Color.White;
+            this.btnShahr.Location = new System.Drawing.Point(172, 75);
+            this.btnShahr.Name = "btnShahr";
+            this.btnShahr.Size = new System.Drawing.Size(112, 37);
+            this.btnShahr.TabIndex = 2;
+            this.btnShahr.Text = "شهر";
+            this.btnShahr.UseVisualStyleBackColor = false;
+            // 
+            // btnOstan
+            // 
+            this.btnOstan.BackColor = System.Drawing.Color.Teal;
+            this.btnOstan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOstan.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnOstan.ForeColor = System.Drawing.Color.White;
+            this.btnOstan.Location = new System.Drawing.Point(292, 75);
+            this.btnOstan.Name = "btnOstan";
+            this.btnOstan.Size = new System.Drawing.Size(112, 37);
+            this.btnOstan.TabIndex = 3;
+            this.btnOstan.Text = "استان";
+            this.btnOstan.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.Teal;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Location = new System.Drawing.Point(534, 75);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(112, 37);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "ویرایش";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Teal;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(414, 75);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 37);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "حذف مخاطب";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Teal;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(655, 75);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(112, 37);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "افزودن مخاطب";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnDarbareyMa
+            // 
+            this.btnDarbareyMa.BackColor = System.Drawing.Color.Teal;
+            this.btnDarbareyMa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDarbareyMa.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnDarbareyMa.ForeColor = System.Drawing.Color.White;
+            this.btnDarbareyMa.Location = new System.Drawing.Point(51, 75);
+            this.btnDarbareyMa.Name = "btnDarbareyMa";
+            this.btnDarbareyMa.Size = new System.Drawing.Size(112, 37);
+            this.btnDarbareyMa.TabIndex = 7;
+            this.btnDarbareyMa.Text = "در باره ما";
+            this.btnDarbareyMa.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("B Yekan", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(356, 19);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(219, 32);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "دفترچه تلفن زنگ کلاس";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PhoneBook.Properties.Resources.Colorful_Circle_Simple_Background_Image_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(930, 477);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDarbareyMa);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnOstan);
+            this.Controls.Add(this.btnShahr);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnShahr;
+        private System.Windows.Forms.Button btnOstan;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDarbareyMa;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
